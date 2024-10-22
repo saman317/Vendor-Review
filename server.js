4a55bd38384c409f69fef93bd3278c0d9b7349a1
saman317 <samanaman317@gmail.com>
2024-10-21 20:07:36 -0500

    port change and css changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const path=require("path");
 
 const authController = require('./controllers/auth.js');
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI);
 
